90ca3eda938c724b2ccaf63288b18c80289a6ff5
Jiaqi Sheng <jsheng@bbtv.com>
2017-01-18 09:17:47 -0800

    Initial commit

---
+++ search tweets by topic.py
@@ -0,0 +1,26 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+ckey = "XXX"
+csecret = "XXXX"
+atoken = "XXXXXX"
+asecret = "XXXXXXX"
+
+
+class listener(StreamListener):
+    def on_data(self, data):
+        all_data = json.loads(data)
+        tweet = all_data["text"]
+        username = all_data["user"]["screen_name"]
+        print(username, tweet)
+        return True
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["election"])
